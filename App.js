228eeabb09bbec54a62eb19c3183a6ad8d11ea6a
paul <m7paul29@gmailcom>
2017-12-10 18:53:32 +0200

    mail suport

--- App.js
+++ App.js
@@ -9,8 +9,14 @@ import {
   Platform,
   StyleSheet,
   Text,
-  View
+  TextInput,
+  Button,
+  View,
+  AsyncStorage,
+  Alert
 } from 'react-native';
+
+import Mailer from 'react-native-mail';
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' +
@@ -20,22 +26,103 @@ const instructions = Platform.select({
 });
 
 export default class App extends Component<{}> {
+
+  constructor(props) {
+    super(props);
+    this.state = { 
+      name: '',
+      descripion: '',
+      year: '',
+      category: '',
+    };
+  }
+  
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
+      <View style={{padding: 10}}>
+      <TextInput
+        style={styles.input}
+        placeholder="Name"
+        value={this.state.name}
+        onChangeText={(name) => this.setState({name})}
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Descripion"
+        value={this.state.descripion}
+        onChangeText={(descripion) => this.setState({descripion})}
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Year"
+        value={this.state.year}
+        onChangeText={(year) => this.setState({year})}
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Category"
+        value={this.state.category}
+        onChangeText={(category) => this.setState({category})}
+      />
+      <Text
+      style = {{padding : 10 }}
+      />
+      <Button
+        onPress={() => this.saveData(this.state.name,this.state.year,this.state.descripion,this.state.category)}
+        title="Save Car"
+      />
+       <Text
+      style = {{padding : 10 }}
+      />
+      <Button
+        onPress={() =>this.sendEmail(this.state.name,this.state.year,this.state.descripion,this.state.category)}
+        title="Send Email"
+        />
+      <Text
+      style = {{padding : 10 }}
+      />
+      <Button
+        onPress={() => this.showAll()}
+        title="Show all Cars"
+        />
       </View>
-    );
-  }
+  );
 }
+
+  saveData(){
+
+  }
+  showAll(){
+
+  }
+
+  sendEmail(name,year,descripion,category){
+    Mailer.mail({
+    subject: 'Selected car',
+    recipients: ['support@example.com'],
+    body: '<b>'+name+"</b><p> Year: "+year+"</p><p> Description: "+descripion+"</p><p> Category:"+ category+'</p>',
+    isHTML: true,
+    attachment: {
+      path: '',  // The absolute path of the file from which to read data.
+      type: '',   // Mime Type: jpg, png, doc, ppt, html, pdf
+      name: '',   // Optional: Custom filename for attachment
+    }
+  }, (error, event) => {
+    Alert.alert(
+      error,
+      event,
+      [
+        {text: 'Ok', onPress: () => console.log('OK: Email Error Response')},
+        {text: 'Cancel', onPress: () => console.log('CANCEL: Email Error Response')}
+      ],
+      { cancelable: true }
+    )
+  });
+  }
+
+}
+
+
 
 const styles = StyleSheet.create({
   container: {
